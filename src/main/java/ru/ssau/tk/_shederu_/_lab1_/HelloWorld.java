5af787ce631f60df54f805c21ae161f48bd15c09
bulatsn <kbnn228@gmail.com>
2025-09-22 19:58:06 +0400

    41

--- src/main/java/ru/ssau/tk/_shederu_/_lab1_/HelloWorld.java
+++ src/main/java/ru/ssau/tk/_shederu_/_lab1_/HelloWorld.java
@@ -2,6 +2,6 @@ package ru.ssau.tk._shederu_._lab1_;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Barakabama");
     }
 }
