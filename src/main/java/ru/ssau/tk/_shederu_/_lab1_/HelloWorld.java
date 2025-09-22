07fc7003d5dc018861144053e12256d7b07f0b88
shederu <lagereva.lis@gmail.com>
2025-09-22 19:59:46 +0400

    Пробуем

--- src/main/java/ru/ssau/tk/_shederu_/_lab1_/HelloWorld.java
+++ src/main/java/ru/ssau/tk/_shederu_/_lab1_/HelloWorld.java
@@ -2,6 +2,6 @@ package ru.ssau.tk._shederu_._lab1_;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Barakabama");
+        System.out.println("Булатсн хорош");
     }
 }
