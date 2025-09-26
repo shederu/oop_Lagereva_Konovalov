62cfa780eee51687e751937eea1371c477ac17d2
shederu <lagereva.lis@gmail.com>
2025-09-26 23:32:48 +0400

    Создание функций ZeroFunction.java и UnitFunction.java

---
+++ src/main/java/ru/ssau/tk/_shederu_/_lab1_/functions/UnitFunction.java
@@ -0,0 +1,7 @@
+package ru.ssau.tk._shederu_._lab1_.functions;
+
+public class UnitFunction extends ConstantFunction{
+    public UnitFunction(){
+        super(1.0);
+    }
+}
---
+++ src/main/java/ru/ssau/tk/_shederu_/_lab1_/functions/ZeroFunction.java
@@ -0,0 +1,7 @@
+package ru.ssau.tk._shederu_._lab1_.functions;
+
+public class ZeroFunction extends ConstantFunction{
+    public ZeroFunction(){
+        super(0.0);
+    }
+}
