a5e87434c1064017e265ccd1602119b73771a8ac
shederu <lagereva.lis@gmail.com>
2025-09-28 23:00:50 +0400

    создание CompositeFunction andThen

--- src/main/java/ru/ssau/tk/_shederu_/_lab1_/functions/MathFunctions.java
+++ src/main/java/ru/ssau/tk/_shederu_/_lab1_/functions/MathFunctions.java
@@ -2,4 +2,8 @@ package ru.ssau.tk._shederu_._lab1_.functions;
 
 interface MathFunctions{
     double apply(double x);
+
+    default MathFunctions andThen(MathFunctions afterFunction) {
+        return new CompositeFunction(this, afterFunction);
+    }
 }
