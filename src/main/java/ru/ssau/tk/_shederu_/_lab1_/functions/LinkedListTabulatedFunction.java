c61808eebaecff3b1d4d64994d00f4d7aa8f32ba
bulatsn <kbnn228@gmail.com>
2025-09-29 23:16:28 +0400

    я пошутил

--- src/main/java/ru/ssau/tk/_shederu_/_lab1_/functions/LinkedListTabulatedFunction.java
+++ src/main/java/ru/ssau/tk/_shederu_/_lab1_/functions/LinkedListTabulatedFunction.java
@@ -1,8 +1,9 @@
 package ru.ssau.tk._shederu_._lab1_.functions;
 
-public class LinkedListTabulatedFunction extends AbstractTabulatedFunction implements Removable, Insertable {
+public class LinkedListTabulatedFunction extends AbstractTabulatedFunction implements Removable {
+    private static final double eRate = 1e-9;
 
-    private static class Node{
+    private static class Node {
         private Node next;
         private Node prev;
         private double x;
@@ -17,48 +18,43 @@ public class LinkedListTabulatedFunction extends AbstractTabulatedFunction imple
     private Node head = null;
     private int count = 0;
 
-
-
-private void addNode(double x, double y){
-    Node nNode = new Node(x, y);
-    if(head == null){
-        head = nNode;
-        head.prev = head;
-        head.next = head;
-    }
-    else{
-        Node last = head.prev;
-        last.next = nNode;
-        head.prev = nNode;
-        nNode.next = head;
-        nNode.next = last;
-    }
-
-    ++count;
+    private void addNode(double x, double y) {
+        Node nNode = new Node(x, y);
+        if (head == null) {
+            head = nNode;
+            head.prev = head;
+            head.next = head;
+        } else {
+            Node last = head.prev;
+            last.next = nNode;
+            nNode.prev = last;
+            nNode.next = head;
+            head.prev = nNode;
+        }
+        ++count;
     }
 
-    public LinkedListTabulatedFunction(double[] xValues, double[] yValues){
+    public LinkedListTabulatedFunction(double[] xValues, double[] yValues) {
+        if (xValues.length != yValues.length) {
+            throw new IllegalArgumentException("Arrays must have same length");
+        }
+        if (xValues.length < 2) {
+            throw new IllegalArgumentException("At least 2 points required");
+        }
         for (int i = 0; i < xValues.length; ++i) {
             addNode(xValues[i], yValues[i]);
         }
-
     }
 
-    public LinkedListTabulatedFunction(MathFunctions source, double xFrom, double xTo, int count){
-        if(xFrom>xTo){
+    public LinkedListTabulatedFunction(MathFunctions source, double xFrom, double xTo, int count) {
+        if (count < 2) {
+            throw new IllegalArgumentException("At least 2 points required");
+        }
+        if (xFrom > xTo) {
             double t = xFrom;
             xFrom = xTo;
             xTo = t;
         }
-
-        if(xTo == xFrom){
-            int i = 0;
-            while(i < count){
-                addNode(xFrom, source.apply(xFrom));
-                ++i;
-            }
-        }
-
 
         double step = (xTo - xFrom) / (count - 1);
         double x = xFrom;
@@ -68,163 +64,143 @@ private void addNode(double x, double y){
             x += step;
         }
     }
-    private Node getNode(int index){
-        Node p=head;
-        for(int i = 0; i<index; ++i){
-               p=p.next;
+
+    private Node getNode(int index) {
+        if (index < 0 || index >= count) {
+            throw new IllegalArgumentException("Index out of bounds");
+        }
+        Node p = head;
+        for (int i = 0; i < index; ++i) {
+            p = p.next;
         }
         return p;
     }
 
-
     @Override
-    public int getCount() { return count; }
-
-    @Override
-    public double leftBound() { return head.x; }
-
-    @Override
-    public double rightBound() { return head.prev.x; }
-
-    @Override
-    public double getX(int index) { return getNode(index).x; }
-
-    @Override
-    public double getY(int index) { return getNode(index).y; }
-
-    @Override
-    public void setY(int index, double value){ getNode(index).y = value; }
-
-    @Override
-    public int indexOfX(double x){
-    Node p = head;
-    int i = 0;
-        do {
-           if(p.x == x){
-               return i;
-           }
-           p = p.next;
-           ++i;
-        } while(p.next!=head);
-       return -1;
-    }
-
-    @Override
-    public int indexOfY(double y){
-        Node p = head;
-        int i = 0;
-        do {
-            if(p.y == y){
-                return i;
-            }
-            p = p.next;
-            ++i;
-        } while(p.next!=head);
-        return -1;
-    }
-
-
-    @Override
-    protected int floorIndexOfX(double x){
-
-    if(head.x>x) {
-        return 0;
-    }
-        Node p = head;
-        int i = 0;
-
-        do{
-            if(p.x>=x){
-                return i -1 ;
-            }
-            p=p.next;
-            ++i;
-        }while(p!=head);
-
+    public int getCount() {
         return count;
     }
 
     @Override
-    protected double extrapolateLeft(double x){
-        if(head.next == head) return head.y;
-        Node f = head;
-        Node s = head.next;
-
-        return f.y + (s.y-f.y)/(s.x-f.x)*(x- f.x);
+    public double leftBound() {
+        return head.x;
     }
 
     @Override
-    protected double extrapolateRight(double x){
-        if(head.next == head) return head.y;
-        Node s = head.prev;
-        Node f = s.prev;
+    public double rightBound() {
+        return head.prev.x;
+    }
 
-        return f.y + (s.y-f.y)/(s.x-f.x)*(x- f.x);
+    @Override
+    public double getX(int index) {
+        return getNode(index).x;
+    }
+
+    @Override
+    public double getY(int index) {
+        return getNode(index).y;
+    }
+
+    @Override
+    public void setY(int index, double value) {
+        getNode(index).y = value;
+    }
+
+    @Override
+    public int indexOfX(double x) {
+        if (head == null) return -1;
+        Node p = head;
+        int i = 0;
+        do {
+            if (Math.abs(p.x - x) < eRate) {
+                return i;
+            }
+            p = p.next;
+            ++i;
+        } while (p != head);
+        return -1;
+    }
+
+    @Override
+    public int indexOfY(double y) {
+        if (head == null) return -1;
+        Node p = head;
+        int i = 0;
+        do {
+            if (Math.abs(p.y - y) < eRate) {
+                return i;
+            }
+            p = p.next;
+            ++i;
+        } while (p != head);
+        return -1;
+    }
+
+    @Override
+    protected int floorIndexOfX(double x) {
+        if (head == null) return 0;
+        if (x < head.x) return 0;
+        if (x > head.prev.x) return count - 1;
+
+        Node current = head;
+        int index = 0;
+        while (index < count - 1) {
+            if (x >= current.x && x < current.next.x) {
+                return index;
+            }
+            current = current.next;
+            index++;
+        }
+        return count - 1;
+    }
+
+    @Override
+    protected double extrapolateLeft(double x) {
+        if (head.next == head) return head.y;
+        Node first = head;
+        Node second = head.next;
+        return interpolate(x, first.x, second.x, first.y, second.y);
+    }
+
+    @Override
+    protected double extrapolateRight(double x) {
+        if (head.next == head) return head.y;
+        Node last = head.prev;
+        Node secondLast = last.prev;
+        return interpolate(x, secondLast.x, last.x, secondLast.y, last.y);
     }
 
     @Override
     protected double interpolate(double x, int floorIndex) {
-        if(head.next == head) return head.y;
-        Node lNode = getNode(floorIndex);
-        Node rNode = getNode(floorIndex + 1);
-        return lNode.y + (rNode.y - lNode.y) / (rNode.x - lNode.x) * (x - lNode.x);
+        Node left = getNode(floorIndex);
+        Node right = getNode(floorIndex + 1);
+        return interpolate(x, left.x, right.x, left.y, right.y);
     }
 
-    protected double interpolate(double x, double leftX, double rightX, double leftY, double rightY) { return leftY + (rightY - leftY) / (rightX - leftX) * (x - leftX); }
+    @Override
+    protected double interpolate(double x, double leftX, double rightX, double leftY, double rightY) {
+        return leftY + (rightY - leftY) / (rightX - leftX) * (x - leftX);
+    }
 
     @Override
     public void remove(int index) {
-        Node nToRem = getNode(index);
+        if (count == 1) {
+            head = null;
+            count = 0;
+            return;
+        }
 
-        Node prevNode = nToRem.prev;
-        Node nextNode = nToRem.next;
+        Node nodeToRemove = getNode(index);
+        Node prevNode = nodeToRemove.prev;
+        Node nextNode = nodeToRemove.next;
 
         prevNode.next = nextNode;
         nextNode.prev = prevNode;
 
-        if (nToRem == head) {
+        if (nodeToRemove == head) {
             head = nextNode;
         }
 
         count--;
-    }
-
-    @Override
-    public void insert(double x, double y) {
-        if (head == null) {
-            addNode(x, y);
-            return;
-        }
-
-        Node current = head;
-        do {
-            if (current.x == x) {
-                current.y = y;
-                return;
-            }
-            current = current.next;
-        } while (current != head);
-
-        current = head;
-        Node newNode = new Node(x, y);
-
-        do {
-            if (current.x > x) {
-                break;
-            }
-            current = current.next;
-        } while (current != head);
-
-        Node prevNode = current.prev;
-
-        newNode.next = current;
-        newNode.prev = prevNode;
-        prevNode.next = newNode;
-        current.prev = newNode;
-
-        if (current == head && head.x > x) {
-            head = newNode;
-        }
-        count++;
     }
 }
