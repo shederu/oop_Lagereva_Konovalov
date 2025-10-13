4a014af7e72d330040230cce1c829ca27fb6e9cd
Konovalov <kbnn228@gmail.com>
2025-10-13 22:25:33 +0400

    я закончил

---
+++ src/main/java/ru/ssau/tk/_shederu_/_lab1_/io/LinkedListTabulatedFunctionSerialization.java
@@ -0,0 +1,53 @@
+package ru.ssau.tk._shederu_._lab1_.io;
+
+import ru.ssau.tk._shederu_._lab1_.functions.LinkedListTabulatedFunction;
+import ru.ssau.tk._shederu_._lab1_.functions.TabulatedFunction;
+import ru.ssau.tk._shederu_._lab1_.operations.TabulatedDifferentialOperator;
+import java.io.*;
+
+public class LinkedListTabulatedFunctionSerialization {
+    public static void main(String[] args) {
+
+        double[] xValues = {0.0, 1.0, 2.0, 3.0, 4.0};
+        double[] yValues = {0.0, 1.0, 4.0, 9.0, 16.0};
+        LinkedListTabulatedFunction originalFunction = new LinkedListTabulatedFunction(xValues, yValues);
+
+        TabulatedDifferentialOperator differentialOperator = new TabulatedDifferentialOperator();
+
+        TabulatedFunction firstDerivative = differentialOperator.derive(originalFunction);
+        TabulatedFunction secondDerivative = differentialOperator.derive(firstDerivative);
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream("output/serialized linked list functions.bin");
+             BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream)) {
+
+            FunctionsIO.serialize(bufferedOutputStream, originalFunction);
+            FunctionsIO.serialize(bufferedOutputStream, firstDerivative);
+            FunctionsIO.serialize(bufferedOutputStream, secondDerivative);
+
+            System.out.println("Функции успешно сериализованы в файл");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (FileInputStream fileInputStream = new FileInputStream("output/serialized linked list functions.bin");
+             BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream)) {
+
+            TabulatedFunction deserializedOriginal = FunctionsIO.deserialize(bufferedInputStream);
+            TabulatedFunction deserializedFirstDerivative = FunctionsIO.deserialize(bufferedInputStream);
+            TabulatedFunction deserializedSecondDerivative = FunctionsIO.deserialize(bufferedInputStream);
+
+            System.out.println("\nИсходная функция:");
+            System.out.println(deserializedOriginal.toString());
+
+            System.out.println("\nПервая производная:");
+            System.out.println(deserializedFirstDerivative.toString());
+
+            System.out.println("\nВторая производная:");
+            System.out.println(deserializedSecondDerivative.toString());
+
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
