3f2cbadaaced5ac7ea611a4c496a99b0d096945b
Lagereva <lagereva.lis@gmail.com>
2025-10-13 20:00:42 +0400

    добавление FunctionsIO

---
+++ src/main/java/ru/ssau/tk/_shederu_/_lab1_/io/FunctionsIO.java
@@ -0,0 +1,7 @@
+package ru.ssau.tk._shederu_._lab1_.io;
+
+public class FunctionsIO {
+    private FunctionsIO() {
+        throw new UnsupportedOperationException("Нельзя создать экземпляр класса FunctionsIO");
+    }
+}
