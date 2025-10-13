8fa5c5abdacb71dc046d1d865b779ef569a13cd0
Lagereva <lagereva.lis@gmail.com>
2025-10-13 19:31:27 +0400

    Добавление исключения

---
+++ src/main/java/ru/ssau/tk/_shederu_/_lab1_/exceptions/InconsistentFunctionsException.java
@@ -0,0 +1,7 @@
+package ru.ssau.tk._shederu_._lab1_.exceptions;
+
+public class InconsistentFunctionsException extends RuntimeException {
+    public InconsistentFunctionsException() { super(); }
+
+    public InconsistentFunctionsException(String message) { super(message);}
+}
