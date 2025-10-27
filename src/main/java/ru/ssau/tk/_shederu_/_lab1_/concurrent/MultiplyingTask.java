7eabbf1c4fd96cfeea3b398e092927ad800a728e
shederu <lagereva.lis@gmail.com>
2025-10-27 18:38:25 +0400

    разработка класса MultiplyingTask

--- src/main/java/ru/ssau/tk/_shederu_/_lab1_/concurrent/MultiplyingTask.java
+++ src/main/java/ru/ssau/tk/_shederu_/_lab1_/concurrent/MultiplyingTask.java
@@ -1,4 +1,22 @@
 package ru.ssau.tk._shederu_._lab1_.concurrent;
 
-public class MultiplyingTask {
+import ru.ssau.tk._shederu_._lab1_.functions.TabulatedFunction;
+
+public class MultiplyingTask implements Runnable{
+    private final TabulatedFunction function;
+
+    public MultiplyingTask(TabulatedFunction function){
+        this.function = function;
+    }
+
+    @Override
+    public void run(){
+        for (int i = 0; i < function.getCount(); i++) {
+            synchronized (function) {
+                double currentY = function.getY(i);
+                function.setY(i, currentY * 2);
+            }
+        }
+        System.out.println("Поток " + Thread.currentThread().getName() + " закончил выполнение задачи.");
+    }
 }
