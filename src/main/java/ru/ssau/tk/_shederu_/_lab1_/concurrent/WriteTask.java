fa4a9715289dee1e6a9148308975e84a044bbecb
Konovalov <kbnn228@gmail.com>
2025-10-27 21:00:33 +0400

    починил для синхронизации

--- src/main/java/ru/ssau/tk/_shederu_/_lab1_/concurrent/WriteTask.java
+++ src/main/java/ru/ssau/tk/_shederu_/_lab1_/concurrent/WriteTask.java
@@ -1,22 +1,31 @@
 package ru.ssau.tk._shederu_._lab1_.concurrent;
 
+
 import ru.ssau.tk._shederu_._lab1_.functions.TabulatedFunction;
 
 public class WriteTask implements Runnable {
-    private  TabulatedFunction func;
+    private TabulatedFunction func;
     private final double value;
+    private Object lock;
 
-    public WriteTask(TabulatedFunction func, double value) {
+    public WriteTask(TabulatedFunction func, double value, Object lock) {
         this.func = func;
         this.value = value;
+        this.lock = lock;
     }
-
     @Override
     public void run() {
-        int count = func.getCount();
-        for (int i = 0; i < count; i++) {
-            func.setY(i, value);
-            System.out.printf("Writing for index %d complete", i);
+        try {
+            for (int i = 0; i < func.getCount(); i++) {
+                synchronized (lock) {
+                    func.setY(i, value);
+                    System.out.printf("Writing for index %d complete%n", i);
+                }
+
+                Thread.sleep(1);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
