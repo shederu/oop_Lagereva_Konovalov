5764ada65ae7d61d131943cc697162ea6f710aab
Konovalov <kbnn228@gmail.com>
2025-11-17 12:55:40 +0400

    глянь

--- src/main/java/ru/ssau/tk/_shederu_/_lab1_/functions/AbstractTabulatedFunction.java
+++ src/main/java/ru/ssau/tk/_shederu_/_lab1_/functions/AbstractTabulatedFunction.java
@@ -1,5 +1,5 @@
 package ru.ssau.tk._shederu_._lab1_.functions;
-
+//работает??
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import ru.ssau.tk._shederu_._lab1_.exceptions.ArrayIsNotSortedException;
