838f9477a80db677b1dd40a4fd063a003a0c5fae
shederu <lagereva.lis@gmail.com>
2025-11-18 22:49:13 +0400

    create_tables.sql

--- src/main/resources/sql/create_tables.sql
+++ src/main/resources/sql/create_tables.sql
@@ -4,16 +4,18 @@ CREATE TABLE "user" (
     password VARCHAR(255) NOT NULL
 );
 
-
-
 CREATE TABLE tabulated_function (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     data BYTEA NOT NULL,
-    derivative BYTEA NOT NULL
+    derivative BYTEA NOT NULL,
+    user_id BIGINT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES "user"(id) ON DELETE CASCADE
 );
 
 CREATE TABLE composite_function (
     id BIGSERIAL PRIMARY KEY,
-    expression TEXT NOT NULL
+    expression TEXT NOT NULL,
+    user_id BIGINT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES "user"(id) ON DELETE CASCADE
 );
