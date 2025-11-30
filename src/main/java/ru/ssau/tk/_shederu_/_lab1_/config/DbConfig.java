bcdc750e32aee15750e14d4d3a2f5b7a2d7fbc56
Konovalov <kbnn228@gmail.com>
2025-11-30 22:56:41 +0400

    вот ща точно пом починил

--- src/main/java/ru/ssau/tk/_shederu_/_lab1_/config/DbConfig.java
+++ src/main/java/ru/ssau/tk/_shederu_/_lab1_/config/DbConfig.java
@@ -3,5 +3,5 @@ package ru.ssau.tk._shederu_._lab1_.config;
 public class DbConfig {
     public static final String DB_URL = "jdbc:postgresql://localhost:5432/OOPs";
     public static final String DB_USER = "postgres";
-    public static final String DB_PASSWORD = "236514"; //
+    public static final String DB_PASSWORD = "236514";
 }
--- src/main/java/ru/ssau/tk/_shederu_/_lab1_/servlet/TabulatedFunctionServlet.java
+++ src/main/java/ru/ssau/tk/_shederu_/_lab1_/servlet/TabulatedFunctionServlet.java
@@ -7,11 +7,12 @@ import ru.ssau.tk._shederu_._lab1_.config.DbConfig;
 import ru.ssau.tk._shederu_._lab1_.dto.TabulatedFunctionDto;
 import ru.ssau.tk._shederu_._lab1_.entities.TabulatedFunctionEntity;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
