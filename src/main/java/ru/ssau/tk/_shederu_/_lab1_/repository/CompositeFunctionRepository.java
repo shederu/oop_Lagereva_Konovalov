9f8c5a7cdc3af048b94961c1aecb962de8a25c00
Konovalov <kbnn228@gmail.com>
2025-11-30 21:58:47 +0400

    у тебя это есть, но у меня не появляется

---
+++ src/main/java/ru/ssau/tk/_shederu_/_lab1_/repository/CompositeFunctionRepository.java
@@ -0,0 +1,27 @@
+package ru.ssau.tk._shederu_._lab1_.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.ssau.tk._shederu_._lab1_.entities.CompositeFunctionEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CompositeFunctionRepository extends JpaRepository<CompositeFunctionEntity, Long> {
+    Optional<CompositeFunctionEntity> findByExpression(String expression);
+
+    List<CompositeFunctionEntity> findByExpressionContaining(String pattern);
+
+    List<CompositeFunctionEntity> findByUserId(Long userId);
+
+    List<CompositeFunctionEntity> findByExpressionContainingAndUserId(String pattern, Long userId);
+
+    boolean existsByExpression(String expression);
+
+    void deleteByExpression(String expression);
+
+    void deleteByUserId(Long userId);
+
+    long countByUserId(Long userId);
+}
