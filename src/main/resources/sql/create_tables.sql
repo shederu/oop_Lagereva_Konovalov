dfb9b798f369f07c43f8bdc92f81535d4c6db56b
Konovalov <kbnn228@gmail.com>
2025-12-16 16:19:23 +0400

    dc` gjlrk.xbk

--- src/main/resources/sql/create_tables.sql
+++ src/main/resources/sql/create_tables.sql
@@ -1,4 +1,5 @@
-CREATE TABLE "user" (
+
+CREATE TABLE users (
     id BIGSERIAL PRIMARY KEY,
     login VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
@@ -10,12 +11,30 @@ CREATE TABLE tabulated_function (
     data BYTEA NOT NULL,
     derivative BYTEA NOT NULL,
     user_id BIGINT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES "user"(id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
 CREATE TABLE composite_function (
     id BIGSERIAL PRIMARY KEY,
     expression TEXT NOT NULL,
     user_id BIGINT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES "user"(id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
+
+CREATE TABLE role (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE user_roles (
+    user_id BIGINT NOT NULL,
+    role_id BIGINT NOT NULL,
+    PRIMARY KEY (user_id, role_id),
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE
+);
+
+
+INSERT INTO role (name) VALUES ('ADMIN');
+INSERT INTO role (name) VALUES ('CREATOR');
+INSERT INTO role (name) VALUES ('VIEWER');
